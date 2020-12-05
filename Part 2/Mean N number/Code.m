042ed025c563dfa65eb15710d991cfb4052dff2e
Mohammad Sabouri <mohamadsabory@yahoo.com>
2020-12-05 05:33:03 +0330

    Mean N number

---
+++ Part 2/Mean N number/Code.m
@@ -0,0 +1,21 @@
+% This code created by "Mohammad Sabouri"
+% December 2020
+%----------------------------------------------------------
+% Linkedin:   https://www.linkedin.com/in/mohammadsabouri/
+% Github:     https://github.com/sabouri1994
+%----------------------------------------------------------
+% Program Description:
+% A program that takes n numbers and calculates their average
+%%
+
+clc  % clear command window
+clear all  % clear Workspace
+%%
+k=0;
+N=input('Enter a count of number :');
+for i=0:1:N
+    A=input('Enter a number :');
+    K=K+A;
+end
+G=K/N;
+fprintf('the average of number is=(%d)',G);
