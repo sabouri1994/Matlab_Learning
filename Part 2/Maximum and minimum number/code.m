82f329f009d2d267fd6427b72257acda95039bc3
Mohammad Sabouri <mohamadsabory@yahoo.com>
2020-12-06 12:16:19 +0330

    Maximum and minimum number

---
+++ Part 2/Maximum and minimum number/code.m
@@ -0,0 +1,27 @@
+% This code created by "Mohammad Sabouri"
+% December 2020
+%----------------------------------------------------------
+% Linkedin:   https://www.linkedin.com/in/mohammadsabouri/
+% Github:     https://github.com/sabouri1994
+%----------------------------------------------------------
+% Program Description:
+% A program that reads 20 numbers and calculates and prints the maximum and minimum of those numbers
+%%
+
+clc  % clear command window
+clear all  % clear Workspace
+%%
+
+A=input('Enter a number:');
+max=A;
+min=A;
+for i=2:1:20
+    C=input('Enter a number:');
+    if (C>max)
+        max=C;
+    elseif (C<min)
+        min=C;
+    end
+end
+
+fprintf('The maximum number is=(%d) and minimum is= (%d)',max,min);
