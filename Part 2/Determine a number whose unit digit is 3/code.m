9497fae23952d38ad9fc02059a8e28bb6bc5fc62
Mohammad Sabouri <mohamadsabory@yahoo.com>
2020-12-06 12:22:27 +0330

    Code

---
+++ Part 2/Determine a number whose unit digit is 3/code.m
@@ -0,0 +1,24 @@
+% This code created by "Mohammad Sabouri"
+% December 2020
+%----------------------------------------------------------
+% Linkedin:   https://www.linkedin.com/in/mohammadsabouri/
+% Github:     https://github.com/sabouri1994
+%----------------------------------------------------------
+% Program Description:
+% A program that reads 20 numbers and calculates and prints the maximum and minimum of those numbers
+%%
+
+clc  % clear command window
+clear all  % clear Workspace
+%%
+
+N=input('Enter a upper bound:');
+for i=1:1:N
+    A=input('Enter a number:');
+    P=rem(A,10);
+    if (P==3)
+        disp('The last digit of number is 3'0;
+        A
+    end
+end
+
